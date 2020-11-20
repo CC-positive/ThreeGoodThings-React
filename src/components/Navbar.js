025b6b894e76dac492430fa9a050667a9a1a9b3c
kazzzzzzaki <kazuakiu0327@gmail.com>
2020-11-20 19:06:35 +0900

    changed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,17 @@ import img1 from "../image/threetter.PNG";
 import AppBar from "@material-ui/core/AppBar";
 import Grid from "@material-ui/core/Grid";
 
+
 function Navbar() {
   const [imgData, setImgData] = useState(img1);
 
   return (
     <AppBar position="static" className="Navbar">
-      <Grid item xs={2}>
-        <img
-          src={imgData}
-          alt="ローディング中"
-          title="空と海"
-          className="lottery"
-        ></img>
-      </Grid>
+        <Grid item xs={2}>
+            <img src={imgData} alt="ローディング中" title="空と海" className="lottery"></img>
+        </Grid>
     </AppBar>
-  );
+);
 }
 
 export default Navbar;
