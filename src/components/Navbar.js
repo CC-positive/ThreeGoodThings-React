c13139ad3b28960f703ab0dbe100ae0f13c52d70
shota0601 <shota.t0601@gmail.com>
2020-11-20 16:49:32 +0900

    capture commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
+import img1 from '../image/threetter.PNG'
+import AppBar from "@material-ui/core/AppBar";
 
 
 function Navbar() {
+    const [imgData, setImgData] = useState(img1);
+
     return (
         <div className="Navbar">
+            <img src={imgData} alt="ローディング中" title="空と海" className="lottery"></img>
             <p>ここNavbarです</p>
         </div>
     );
--- src/components/TGTInput.js
+++ src/components/TGTInput.js
@@ -1,11 +1,17 @@
 // import { useState, useEffect } from "react";
 import React from "react";
 
+const onButtonClick = () => {
+    console.log("検証")
+};
 
 function TGTInput() {
     return (
         <div className="TGTInput">
             <p>ここTgtInputです</p>
+            <button type="button" value="Upload" onClick={onButtonClick}>
+                Upload
+            </button>
         </div>
     );
 }
