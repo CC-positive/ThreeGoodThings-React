565bdd75a8debeec09d14985563ebdfb353ba510
kazzzzzzaki <kazuakiu0327@gmail.com>
2020-11-20 19:09:02 +0900

    fix conflict

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,13 @@ import Grid from "@material-ui/core/Grid";
 function Navbar() {
   const [imgData, setImgData] = useState(img1);
 
-  return (
-    <AppBar position="static" className="Navbar">
-        <Grid item xs={2}>
-            <img src={imgData} alt="ローディング中" title="空と海" className="lottery"></img>
-        </Grid>
-    </AppBar>
-);
+      return (
+        <AppBar position="static" className="Navbar">
+            <Grid item xs={2}>
+                <img src={imgData} alt="ローディング中" title="空と海" className="lottery"></img>
+            </Grid>
+        </AppBar>
+    );
 }
 
 export default Navbar;
