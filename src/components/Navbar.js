a1b90426dcde9aa02a034fd0fba0aa8a73a46fb1
shota0601 <shota.t0601@gmail.com>
2020-11-20 16:58:12 +0900

    Navber commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,18 @@ import { useState, useEffect } from "react";
 import React from "react";
 import img1 from '../image/threetter.PNG'
 import AppBar from "@material-ui/core/AppBar";
+import Grid from "@material-ui/core/Grid";
 
 
 function Navbar() {
     const [imgData, setImgData] = useState(img1);
 
     return (
-        <div className="Navbar">
-            <img src={imgData} alt="ローディング中" title="空と海" className="lottery"></img>
-            <p>ここNavbarです</p>
-        </div>
+        <AppBar position="static" className="Navbar">
+            <Grid item xs={2}>
+                <img src={imgData} alt="ローディング中" title="空と海" className="lottery"></img>
+            </Grid>
+        </AppBar>
     );
 }
 
