637371138a674f65983e5b3a7c09711d521b21f3
kazzzzzzaki <kazuakiu0327@gmail.com>
2020-11-24 00:23:18 +0900

    changed to list tgt in cards

--- src/components/TGT.js
+++ src/components/TGT.js
@@ -1,13 +1,64 @@
 import { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Divider,
+  Card,
+  CardHeader,
+  CardContent,
+} from "@material-ui/core";
+import ThumbUpAltSharpIcon from "@material-ui/icons/ThumbUpAltSharp";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: "left",
+  },
+}));
 
 function TGT(props) {
+  const classes = useStyles();
   return (
     <>
-      <p>{props.userName}</p>
-      <p>{props.tgt.text1}</p>
-      <p>{props.tgt.text2}</p>
-      <p>{props.tgt.text3}</p>
+      <Card className={classes.root}>
+        <CardHeader title={props.userName} subheader="November 20, 2020" />
+        <CardContent>
+          <List>
+            <Divider variant="inset" component="li" />
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <ThumbUpAltSharpIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={props.tgt.text1} />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <ThumbUpAltSharpIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={props.tgt.text2} />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <ThumbUpAltSharpIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={props.tgt.text3} />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </List>
+        </CardContent>
+      </Card>
     </>
   );
 }
