14dfce490be6c0209551b73f9e6120181297db3d
kazzzzzzaki <kazuakiu0327@gmail.com>
2020-11-23 23:26:54 +0900

    fixed position

--- src/components/TGTInput.js
+++ src/components/TGTInput.js
@@ -26,7 +26,9 @@ const useStyles = makeStyles((theme) => ({
   },
   card: {
     width: 500,
-    marginLeft: "32%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    scrollSnapMarginRight: "auto",
     backgroundColor: blue[100],
     marginBottom: 10,
   },
@@ -82,7 +84,7 @@ function TGTInput(props) {
         <Card className={classes.card}>
           <CardHeader
             className={classes.header}
-            avatar={<Avatar alt="googleUserImg" src={props.imageUrl} />}
+            avatar={<Avatar alt="googleUserImg" src={props.imgUrl} />}
           />
           <CardContent color="red">
             <div className={classes.TGT}>
