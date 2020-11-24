f393219290298f438e8d3f6dbe36b9472cae8e2b
shota0601 <shota.t0601@gmail.com>
2020-11-24 10:33:19 +0900

    renzokutouko commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,21 @@ import Navbar from "./Navbar";
 import TGTInput from "./TGTInput";
 import TGTList from "./TGTList";
 import React from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [updateFlag, setUpdateFlag] = useState("OFF");
+
+  function updateFlagChange() {
+    console.log("qqqqqq");
+    const updateFlag = "ON";
+    setUpdateFlag(updateFlag);
+  }
+  console.log("レンダリング");
   return (
     <div className="App">
       <Navbar />
-      <TGTInput />
+      <TGTInput updateFlagChange={updateFlagChange} />
       <TGTList />
     </div>
   );
