49eafde7f17a7c9710c477a101209c6dec65202d
h-tsuboi918 <hiroki.tsuboi.918@gmail.com>
2020-11-25 14:20:53 +0900

    [add] save google info to cookie

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import {
   useGoogleLogout,
 } from "react-google-login";
 import { config } from "../config";
+import { useCookies } from "react-cookie";
 
 function App() {
   const [toukouState, setToukouState] = useState(0);
@@ -21,6 +22,12 @@ function App() {
   const [imgUrl, setImgUrl] = useState("");
   const [updateFlag, setUpdateFlag] = useState("OFF");
   const [continuous, setContinuous] = useState(0);
+  const [cookies, setCookie, removeCookie] = useCookies([
+    "idToken",
+    "googleId",
+    "userName",
+    "imgUrl",
+  ]);
 
   const CLIENT_ID =
     "535477566115-nk6dj1hrk0gvsfrmhimmbqgts7f3puqt.apps.googleusercontent.com";
@@ -32,6 +39,12 @@ function App() {
       setGoogleId(response.profileObj.googleId);
       setUserName(response.profileObj.name);
       setImgUrl(response.profileObj.imageUrl);
+
+      //save cookie
+      setCookie("googleId", response.profileObj.googleId);
+      setCookie("idToken", response.tokenId);
+      setCookie("userName", response.profileObj.name);
+      setCookie("imgUrl", response.profileObj.imageUrl);
 
       ////login api send
       //set header
@@ -92,6 +105,10 @@ function App() {
     setGoogleId("");
     setUserName("");
     setImgUrl("");
+    removeCookie("googleId");
+    removeCookie("idToken");
+    removeCookie("userName");
+    removeCookie("imgUrl");
   }
 
   const handleLoginFailure = (response) => {
@@ -109,6 +126,26 @@ function App() {
   function updatestate() {
     setToukouState(toukouState + 1);
   }
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const cookieGoogleId = cookies.googleId;
+      const cookieIdToken = cookies.idToken;
+      const cookieUserName = cookies.userName;
+      const cookieImgUrl = cookies.imgUrl;
+      const validatePath = "https://oauth2.googleapis.com/tokeninfo?id_token=";
+      const path = validatePath + cookieIdToken;
+      const authRes = await fetch(path);
+      if (authRes.status === 200) {
+        setGoogleId(cookieGoogleId);
+        setIdToken(cookieIdToken);
+        setUserName(cookieUserName);
+        setImgUrl(cookieImgUrl);
+        setLoginSuccess(true);
+      }
+    };
+    checkLogin();
+  }, []);
 
   return (
     <div className="App">
